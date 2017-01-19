e1b7aaf121d913670e9210a924fa600f056f24ba
Gianni <giangraziano@gmail.com>
2017-01-19 19:48:05 +0100

    added Networ Utilities

--- app/src/main/java/com/example/android/popularmovies/utilities/NetworkUtilities.java
+++ app/src/main/java/com/example/android/popularmovies/utilities/NetworkUtilities.java
@@ -1,10 +1,71 @@
 package com.example.android.popularmovies.utilities;
 
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 /**
  * Created by giannig on 19/01/17.
  */
 
 public class NetworkUtilities {
 
-    prova la cacca;
+    public final static String POPULAR = "popular";
+    public final static String TOP_RATED = "top_rated";
+
+    private final static String TAG = NetworkUtilities.class.getSimpleName();
+    private final static String api_key = "999999999999";
+
+    final static String QUERY_APY_KEY = "api_key";
+    final static String TYPE = "movie";
+
+    //TODO sistemare questo
+    private final static String BASE_URL = "https://api.themoviedb.org/3/";
+
+
+    public static URL BuildUrl(String request){
+        Uri builtUri = Uri.parse(BASE_URL)
+                .buildUpon()
+                .appendEncodedPath(TYPE)
+                .appendEncodedPath(request)
+                .appendQueryParameter(QUERY_APY_KEY, api_key)
+                .build();
+
+        URL url = null;
+        try{
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.v(TAG, "URI: " + url);
+
+        return url;
+    }
+
+    public static String getResponseFromHttp(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream input = connection.getInputStream();
+
+            Scanner scanner = new Scanner(input);
+            scanner.useDelimiter("//A");
+
+            if (scanner.hasNext()) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        }finally {
+            connection.disconnect();
+        }
+
+    }
 }
