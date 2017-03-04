458788dbe2ff061f3d076a1b847a3c789d81c3d5
Gianni <giangraziano@gmail.com>
2017-03-04 17:37:34 +0100

    added trailer list res

---
+++ app/src/main/java/com/example/android/popularmovies/DetailMovieAdapter.java
@@ -0,0 +1,71 @@
+package com.example.android.popularmovies;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.popularmovies.movies.TrailersResults;
+
+/**
+ * Created by giannig on 3/4/17.
+ */
+
+public class DetailMovieAdapter extends RecyclerView.Adapter<DetailMovieAdapter.DetailMovieAdapterViewHolder> {
+
+    private Context ctx;
+    private TrailersResults.Trailer[] mData;
+
+
+    public void setData(TrailersResults.Trailer[] trailers){
+        mData = trailers;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public DetailMovieAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        ctx = parent.getContext();
+
+        LayoutInflater inflater = LayoutInflater.from(ctx);
+        View view = inflater.inflate(R.layout.trailer_item, parent, false);
+
+        return new DetailMovieAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(DetailMovieAdapterViewHolder holder, int position) {
+        TrailersResults.Trailer trailer = mData[position];
+
+        holder.mTrailerTextView.setText(trailer.getMovieName());
+
+        Log.d("ON_BIND_VIEWHOLDER ", trailer.getMovieName());
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mData == null) return 0;
+        return mData.length;
+    }
+
+    class DetailMovieAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public final TextView mTrailerTextView;
+
+        public DetailMovieAdapterViewHolder(View v){
+            super(v);
+
+            mTrailerTextView = (TextView) v.findViewById(R.id.tv_trailer_item);
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            //lanciare l'intent esplicito
+            Log.d("CLICK", "click");
+        }
+    }
+}
